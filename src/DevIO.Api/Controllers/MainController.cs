f69676af7bb6c53c3d3299d9c53fea204f5ef14c
leomazzeu <leonardomazzeudasilva@gmail.com>
2023-04-14 11:30:34 -0300

    Configuração básica das controllers para testar API

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+using DevIO.Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // Validação de notificações e erro
+
+        // Validação de modelstate
+
+        //Validação da operação de negócios
+
+    }
+}
