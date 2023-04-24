7531fdfd76bff59547cc5fd45df79e203a0c10c7
leomazzeu <leonardomazzeudasilva@gmail.com>
2023-04-24 20:38:22 -0300

    Padronização de erros e de negócios

--- src/DevIO.Api/Controllers/MainController.cs
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -1,16 +1,65 @@
+using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
 using DevIO.Data.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace DevIO.Api.Controllers
 {
     [ApiController]
     public abstract class MainController : ControllerBase
     {
-        // Validação de notificações e erro
+        private readonly INotificador _notificador;
 
-        // Validação de modelstate
+        protected MainController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
 
-        //Validação da operação de negócios
+        protected bool OperacaoValida()
+        {
+            return !_notificador.TemNotificacao();
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notificador.ObterNotificacoes().Select(n => n.Mensagem)
+            });
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotificarErroModelInvalida(modelState);
+            return CustomResponse();
+        }
+
+        protected void NotificarErroModelInvalida(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var erro in erros)
+            {
+                var errorMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotificarErro(errorMsg);
+            }
+        }
+
+        protected void NotificarErro(String mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
 
     }
 }
